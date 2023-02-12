aec07459df7adf56639bef30249d827093d6bd13
Manoj Deore <112100835+Manojdeore7@users.noreply.github.com>
2023-02-12 18:52:54 +0530

    final commit

--- demo.js
+++ demo.js
@@ -1,26 +1,14 @@
-// console.log(document.title);
-// console.log(document.all[1])
-// console.log(document.links)
-// console.log(document.forms[0]);
 let header=document.getElementById("header-title");
 console.log(document.doctype);
 header.innerText="Mnaoj Deore";
 header.parentNode.style.border="1px black solid";
-// let list=document.getElementsByClassName("list-group-item");
-// list[2].style.background="green";
-// for(let i=0;i<list.length;i++){
-//     let bold=document.createElement("b");
-//     bold.innerText= list[i].innerText;
-//     list[i].innerText="";
-//     list[i].append(bold);
-    
-// }
-let list=document.getElementByTagName("li");
-li[2].style.background="green";
-for(let i=0;i<li.length;i++){
+let list=document.querySelectorAll(".list-group-item");
+list[2].style.color="red";
+list[1].style.background="green";
+for(let i=0;i<list.length;i++){
     let bold=document.createElement("b");
-    bold.innerText= li[i].innerText;
-    li[i].innerText="";
-    li[i].append(bold);
+    bold.innerText= list[i].innerText;
+    list[i].innerText="";
+    list[i].append(bold);
     
 }
