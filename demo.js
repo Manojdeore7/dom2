7634f3886a2dc8e9878532e7a0832836d8f4d1c9
Manoj Deore <112100835+Manojdeore7@users.noreply.github.com>
2023-02-12 18:27:34 +0530

    dom2 added

---
+++ demo.js
@@ -0,0 +1,17 @@
+// console.log(document.title);
+// console.log(document.all[1])
+// console.log(document.links)
+// console.log(document.forms[0]);
+let header=document.getElementById("header-title");
+console.log(document.doctype);
+header.innerText="Mnaoj Deore";
+header.parentNode.style.border="1px black solid";
+let list=document.getElementsByClassName("list-group-item");
+list[2].style.background="green";
+for(let i=0;i<list.length;i++){
+    let bold=document.createElement("b");
+    bold.innerText= list[i].innerText;
+    list[i].innerText="";
+    list[i].append(bold);
+    
+}
