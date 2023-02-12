ff0878ed3ab241fbf1b27c88a5108250b99a72ca
Manoj Deore <112100835+Manojdeore7@users.noreply.github.com>
2023-02-12 18:36:03 +0530

    2nd demod added

--- demo.js
+++ demo.js
@@ -6,12 +6,21 @@ let header=document.getElementById("header-title");
 console.log(document.doctype);
 header.innerText="Mnaoj Deore";
 header.parentNode.style.border="1px black solid";
-let list=document.getElementsByClassName("list-group-item");
-list[2].style.background="green";
-for(let i=0;i<list.length;i++){
+// let list=document.getElementsByClassName("list-group-item");
+// list[2].style.background="green";
+// for(let i=0;i<list.length;i++){
+//     let bold=document.createElement("b");
+//     bold.innerText= list[i].innerText;
+//     list[i].innerText="";
+//     list[i].append(bold);
+    
+// }
+let list=document.getElementByTagName("li");
+li[2].style.background="green";
+for(let i=0;i<li.length;i++){
     let bold=document.createElement("b");
-    bold.innerText= list[i].innerText;
-    list[i].innerText="";
-    list[i].append(bold);
+    bold.innerText= li[i].innerText;
+    li[i].innerText="";
+    li[i].append(bold);
     
 }
